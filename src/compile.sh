e7022b9938e0b0e9c76ee39f8ca0363e0f85887a
matheus <werneckmatheus12@gmail.com>
2021-01-31 18:04:40 -0300

    Script alterado para somente compilação

--- src/compile.sh
+++ src/compile.sh
@@ -1,10 +1,6 @@
-echo "Digite o caminho do arquivo csv"
-read txt_path
-
 g++ -o main *.cpp
 if [ $? -eq 0 ];then 
    echo "compilado com sucesso!"
-   ./main $txt_path
 else
    echo "nao compilado"
 fi
